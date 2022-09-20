4980a934d18ffa107538ec9f5cdba9ec71cd7fa7
Natã <natagrecu34@gmail.com>
2022-09-20 20:15:01 -0300

    Criar projeto Spring #1
    - Criado estrutura do projeto com framework Spring
    
    Signed-off-by: Natã <natagrecu34@gmail.com>

---
+++ connectFood/src/main/java/br/com/startupone/connectFood/ConnectFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.startupone.connectFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectFoodApplication.class, args);
+	}
+
+}
