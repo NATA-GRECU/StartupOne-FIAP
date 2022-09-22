0a84f63ffd6fb446d5213da192cd3482684a71c5
Natã <natagrecu34@gmail.com>
2022-09-21 22:06:09 -0300

    Subindo Coisas faltantes
    Signed-off-by: Natã <natagrecu34@gmail.com>

--- connectFood/src/main/java/br/com/connectFood/repository/InstituicaoRepository.java
+++ connectFood/src/main/java/br/com/connectFood/repository/InstituicaoRepository.java
@@ -1,2 +1,16 @@
-package br.com.connectFood.repository;public interface InstituicaoRepository {
+package br.com.connectFood.repository;
+
+import br.com.connectFood.models.InstituicaoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InstituicaoRepository extends JpaRepository<InstituicaoModel, Integer> {
+
+    List<InstituicaoModel> findByNomeInstituicao(String nome);
+    List<InstituicaoModel> findByCnpj(String cnpj);
+    List<InstituicaoModel> findByEstado(String estado);
+    List<InstituicaoModel> findByCidade(String cidade);
+    List<InstituicaoModel> findByBairro(String bairro);
+
 }
